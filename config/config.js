14f94ef22fc2e216f38ceb76e0714363f637eb7a
James <jamesgault1488@gmail.com>
2021-03-13 12:35:43 -0500

    fixed eslint issues in config file

--- config/config.js
+++ config/config.js
@@ -4,17 +4,19 @@ module.exports = {
     password: process.env.MySQL,
     database: "passport_demo",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "mysql"
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "mysql"
   },
   production: {
+    // eslint-disable-next-line camel case
+    // eslint-disable-next-line camelcase
     use_env_variable: "JAWSDB_URL",
-    dialect: "mysql",
-  },
+    dialect: "mysql"
+  }
 };
