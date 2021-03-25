5619ca906c166bd832a52aec62812f7b4109269b
James <jamesgault1488@gmail.com>
2021-03-24 21:06:01 -0400

    updating final build

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,8 +24,9 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    // res.render("display");
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render("display", {GAPI_KEY: process.env.api_key});
+    
+    // res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
   // app.get("/recents", (req, res) => {
